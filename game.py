2969161ecf0a5851e5959ce48f1258fd77717949
Kristine <chankristineagcay@gmmail.com>
2022-11-19 12:58:03 +0800

    Implemented enemies

--- game.py
+++ game.py
@@ -53,7 +53,21 @@ class Player(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
         
-    
+class Enemy(Ship):
+    COLOR_MAP = {
+                "red": (RED_SPACE_SHIP, RED_LASER),
+                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+                "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+                }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        
+    def move(self, vel):
+        self.y += vel
+
 
 # Drawing and Getting Main Loop
 
@@ -63,7 +77,13 @@ def main():
     Level = 1
     Lives = 5
     main_font = pygame.font.SysFont ("comicsans", 30)
+    
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+    
     player_vel = 5
+    
     player = Player(300, 650)
     
     ship = Ship (300, 650)
@@ -78,12 +98,25 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        ship.draw(WIN)
+
+        for enemy in enemies:
+            enemy.draw(WIN) 
+            
+        player.draw(WIN)
+        
+   
         pygame.display.update()
         
     while run:
         clock.tick(FPS)
         redraw_window()
+        
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -101,6 +134,9 @@ def main():
         if keys[pygame.K_SPACE]:
             player.shoot()
 
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            
 
 
         
