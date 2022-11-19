ca53626c7e2828a76be8308c87a27664552f4832
Kristine <chankristineagcay@gmmail.com>
2022-11-19 10:30:25 +0800

    Load the images that I'll be used in the game

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+import time
+import random
+
+# Load Images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+# Player Ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
