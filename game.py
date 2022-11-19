cb4341c146cf4af90e1db64d01b5932f1c3f7da6
Kristine <chankristineagcay@gmmail.com>
2022-11-19 10:58:11 +0800

    Create the basic loop for window exit/not

--- game.py
+++ game.py
@@ -25,3 +25,21 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # Background
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
 
+
+# Drawing and Getting Main Loop
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+   
+    while run:
+        clock.tick(FPS)
+        
+# Check for Events (Check is user quit game window )
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
