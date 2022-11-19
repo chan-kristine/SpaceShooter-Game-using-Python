178c93416e5ad7b06bbbf2f543ef584320392dd3
Kristine <chankristineagcay@gmmail.com>
2022-11-19 10:38:16 +0800

    Load the background image

--- game.py
+++ game.py
@@ -11,8 +11,12 @@ BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_smal
 # Player Ship
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
 
-#Lasers
+# Lasers
 RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# Background
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
