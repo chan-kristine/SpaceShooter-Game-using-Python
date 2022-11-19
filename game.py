40e398afc2d396956caf94fe98791cb027b2b00d
Kristine <chankristineagcay@gmmail.com>
2022-11-19 11:49:36 +0800

    Set the lives and level of the game

--- game.py
+++ game.py
@@ -1,20 +1,22 @@
+import faulthandler
 import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 # Set up pygame window
-WIDTH, HEIGHT = 750, 740
+WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aleja's Space Shooter")
 
-# Load Images
+# Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 
-# Player Ship
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
+# Player player
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 # Lasers
 RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
@@ -23,7 +25,7 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # Background
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
 # Drawing and Getting Main Loop
@@ -31,10 +33,25 @@ BG = pygame.image.load(os.path.join("assets", "background-black.png"))
 def main():
     run = True
     FPS = 60
-    clock = pygame.time.Clock()
+    Level = 1
+    Lives = 5
+    main_font = pygame.font.SysFont ("comicsans", 30)
+    clock = pygame.time.Clock
    
+    def redraw_window():
+        WIN.blit (BG,(0,0))
+        # draw text 
+        lives_label = main_font.render(f"Lives: {Lives}" , 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {Level}" , 1, (255, 255, 255))
+        
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        pygame.display.update()
+        
     while run:
         clock.tick(FPS)
+        redraw_window()
         
 # Check for Events (Check is user quit game window )
 
